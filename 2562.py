2cea282dcdcf057110b7270e3ad77fd01ea0a635
ssorlawlgns <ssorlawlgns@naver.com>
2021-07-29 15:28:14 +0900

    백준 2562번 (최댓값)

---
+++ 2562.py
@@ -0,0 +1,12 @@
+# arr=[]
+# for i in range(9):
+#     arr.append(int(input()))
+
+# print(max(arr))
+# print(arr.index(max(arr))+1)
+arr=[]
+for i in range(9):
+    arr.append(int(input()))
+
+print(max(arr))
+print(arr.index(max(arr))+1)
