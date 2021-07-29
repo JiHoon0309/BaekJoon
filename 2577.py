6e441d24c3551288c490eacb69ee80008a334a11
ssorlawlgns <ssorlawlgns@naver.com>
2021-07-29 15:28:29 +0900

    백준 2577번 (숫자의 개수)

---
+++ 2577.py
@@ -0,0 +1,16 @@
+# A=int(input())
+# B=int(input())
+# C=int(input())
+
+# result=list(str(A*B*C))
+# for i in range(10):
+#     print(result.count(str(i)))
+
+
+A=int(input())
+B=int(input())
+C=int(input())
+
+result=list(str(A*B*C))
+for i in range(10):
+    print(result.count(str(i)))
