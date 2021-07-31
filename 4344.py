955898440da5c62ad9b107f6c303fea32822df2c
ssorlawlgns <ssorlawlgns@naver.com>
2021-07-31 23:44:18 +0900

    백준 4344번 (평균은 넘겠지)_01

---
+++ 4344.py
@@ -0,0 +1,13 @@
+C=int(input())
+for i in range(C):
+    sum=0
+    count=0
+    score=list(map(int,input().split()))
+    for j in range(score[0]):
+        sum+=score[j+1]
+    avg=sum/score[0]
+    for z in range(score[0]):
+        if score[z+1]<avg:
+            count+=1
+    a=(count/score[0])*100
+    print(format(a,".3f"),'%')
